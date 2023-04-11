466ad5557037aa4c3d8664053a4b04c3d0de4720
Parimal-Tank <parimalt@zignuts.com>
2023-04-11 18:26:44 +0530

    Environment and Redirect example

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // redirects: async () => {
+  //   return  [
+  //   {
+  //       source:  '/comments',
+  //       destination:  '/',
+  //       permanent:  true  
+  //   }
+  //   ]
+  //   },
 }
 
 module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,10 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
   return (
     <>
-      Hello
+      Hello {process.env.NEXT_PUBLIC_ANALYTICS_ID}
     </>
   )
 }
